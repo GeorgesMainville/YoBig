73456009e858af129c98b858b7f9632c253cc4b0
Etienne Simoneau <etiennesimoneau@hotmail.fr>
2021-05-26 20:02:29 -0400

    PR: Tests grouped with a describe

--- WebApp/src/components/BasicTest/BasicTestContainer.test.tsx
+++ WebApp/src/components/BasicTest/BasicTestContainer.test.tsx
@@ -1,27 +1,29 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import BasicTestContainer from './BasicTestContainer';
 
-test('ChangeQuote_WhenTextValueIsFirstValue_SetQuoteToSecondValue', () => {
-  // Arrange
-  const { baseElement } = render(<BasicTestContainer />);
-  const quoteText = screen.getByText('Georges is the best programmer');
+describe('BasicTestContainer', () => {
+  it('ChangeQuote_WhenTextValueIsFirstValue_SetQuoteToSecondValue', () => {
+    // Arrange
+    const { baseElement } = render(<BasicTestContainer />);
+    const quoteText = screen.getByText('Georges is the best programmer');
 
-  // Act
-  fireEvent.click(screen.getByText('Change quote'));
+    // Act
+    fireEvent.click(screen.getByText('Change quote'));
 
-  // Assert
-  expect(quoteText).toHaveTextContent('Etien is the best programmer');
-});
+    // Assert
+    expect(quoteText).toHaveTextContent('Etien is the best programmer');
+  });
 
-test('ChangeQuote_WhenClickingTwoTimes_RevertQuoteToFirstValue', () => {
-  // Arrange
-  const { baseElement } = render(<BasicTestContainer />);
-  const quoteText = screen.getByText('Georges is the best programmer');
+  it('ChangeQuote_WhenClickingTwoTimes_RevertQuoteToFirstValue', () => {
+    // Arrange
+    const { baseElement } = render(<BasicTestContainer />);
+    const quoteText = screen.getByText('Georges is the best programmer');
 
-  // Act
-  fireEvent.click(screen.getByText('Change quote'));
-  fireEvent.click(screen.getByText('Change quote'));
+    // Act
+    fireEvent.click(screen.getByText('Change quote'));
+    fireEvent.click(screen.getByText('Change quote'));
 
-  // Assert
-  expect(quoteText).toHaveTextContent('Georges is the best programmer');
+    // Assert
+    expect(quoteText).toHaveTextContent('Georges is the best programmer');
+  });
 });
