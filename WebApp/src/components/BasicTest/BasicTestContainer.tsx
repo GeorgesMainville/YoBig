81e98d5cc21bfe3455c493d476cc82827a388b1e
GeorgesMainville <georgesmainville@hotmail.fr>
2021-05-26 22:58:10 -0400

    Use formatted message in basicTestContainer

--- WebApp/src/components/BasicTest/BasicTestContainer.tsx
+++ WebApp/src/components/BasicTest/BasicTestContainer.tsx
@@ -1,24 +1,21 @@
 import './BasicTestContainer.css';
 import { IonText, IonButton } from '@ionic/react';
 import React, { useState } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-  const quotes = [
-    'Georges is the best programmer',
-    'Etien is the best programmer',
-  ];
-
-  const [quote, setQuote] = useState(quotes[0]);
 
   const swapQuote = () => {
-    setQuote(quotes.find((x) => x !== quote)!);
+    // TODO: Set intl
   };
 
   return (
     <div className="container">
-      <IonText color="primary">{quote}</IonText>
+      <IonText color="primary">
+        <FormattedMessage id='app_text' />
+      </IonText>
       <div />
       <IonButton color="secondary" className="quoteButton" onClick={swapQuote}>
         Change quote
